f56b0529db17931b653f466a9bbf35b70dd90a59
Luis Cardoso <luis.cardoso@feedzai.com>
2019-02-20 14:18:36 +0000

    Revert "Deleted credentials"
    This reverts commit 9e5575ef9ef68716bc998b670a39ab7af73198d0.

--- src/utils/rebase.js
+++ src/utils/rebase.js
@@ -3,7 +3,12 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 const app = firebase.initializeApp({
-    /* TODO: Put firebase credentails here */
+    apiKey: "AIzaSyB1MauDH9sMgzKdH1ISxn5SEF8rTvW1n48",
+    authDomain: "my-first-react-app-c702d.firebaseapp.com",
+    databaseURL: "https://my-first-react-app-c702d.firebaseio.com",
+    projectId: "my-first-react-app-c702d",
+    storageBucket: "my-first-react-app-c702d.appspot.com",
+    messagingSenderId: "345905942753"
 });
 
 const db = firebase.database(app);
